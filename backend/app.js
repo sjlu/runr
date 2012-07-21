50c89072554b0e4cd6385036175dbb2ed2ef214d
russfrank <russ.frank@rutgers.edu>
2012-07-21 19:56:11 -0400

    added backend stuff

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,11 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
 var http = require('http');
 
 var app = express();
+
+var nnn = require('nnn');
 
 app.configure(function(){
   app.set('port', process.env.PORT || 8000);
@@ -20,12 +21,30 @@ app.configure(function(){
   app.use(app.router);
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+app.post('/route', function (req, res) {
+  // req.body.locations
+
 });
 
-app.get('/', routes.index);
+/* 
 
+Steve gives me locations and I calculate the distance to the user and place
+them into mongo.  This happens on selection in the interface.
+
+Later, Steve requests 'complete'. The actual computation happens here.  We
+use the calculated distances to do a nearest neighbor algorithm.  We might
+try it multiple times. We request directions from google maps based on the
+lat lons we calculate to be the best 'as the crow flies' path.  We then
+choose the fastest out of those we choose and send the waypoints back to
+Steve.
+
+*/
+
+
+/*
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+*/
+
+module.exports = http.createServer(app);
